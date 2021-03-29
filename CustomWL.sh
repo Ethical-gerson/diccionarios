85f8516b5df9dffa701dd0796df2943d527782dd
Ethical-gerson <43458665+Ethical-gerson@users.noreply.github.com>
2021-03-29 15:29:10 -0300

    Update CustomWL.sh

--- CustomWL.sh
+++ CustomWL.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Recuerda que debes tener python instalado" 
 echo "Iniciando CeWL en" $1 
-wget https://gist.githubusercontent.com/FrankSpierings/8809a737c63059f7447dfb380c1563f9/raw/b5e579ae206e80191fdd332dea77c8049425333b/crewl.py
+wget https://raw.githubusercontent.com/Ethical-gerson/diccionarios/main/crewl.py
 pip install scrapy
 pip3 install scrapy
 timeout 20 python3 crewl.py https://$1 wordlist1
